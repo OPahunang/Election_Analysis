662751e21990dffa412b614a64b65e646114a2f5
OPahunang <olypahunang@yahoo.com>
2022-01-21 23:44:04 -0500

    Adding PyPoll.py file.

--- practice-1.py
+++ practice-1.py
@@ -81,7 +81,6 @@
 #    print (x)
 #    x = x + 1    
 
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-for county, voters in counties_dict.items():
-    print (f"{county} county has {voters} registered voters")
-    
+#counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+#for county, voters in counties_dict.items():
+#    print (f"{county} county has {voters} registered voters")
